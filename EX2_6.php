4e820277fd5ce4e9b9c73490fe3cb841bdabbc4b
eric <ecastellani@outlook.fr>
2022-10-28 16:50:58 +0200

    EX2_6

---
+++ EX2_6.php
@@ -0,0 +1,14 @@
+<?php
+
+$elements = ["Monsieur","Madame","Mademoiselle"];
+function alimenterListeDeroulante ($elements){
+	$result ="";
+	foreach ($elements as $genre){
+		$result .="
+			<for label ='$genre'>$genre</label>
+			<select name = '$genre' id=''>
+			<option value = '$genre'>$genre</option><br>";
+}
+	return $result;
+
+echo alimenterListeDeroulante($elements);}
