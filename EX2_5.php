38d2fe26c674464966739311a301937959817df8
eric <ecastellani@outlook.fr>
2022-10-28 14:52:33 +0200

    EX2_5

---
+++ EX2_5.php
@@ -0,0 +1,28 @@
+<?php
+
+// DECLARATION DES VARIABLES	//
+
+$nomsInput=["Nom","Prénom","Ville"];
+
+
+// function afficherInput ($nomsInput ){
+	//$result ="<input type ='texte'";
+		//foreach($nomsInput as $nominput){
+		//return $result;}}
+
+//echo afficherInput ($nomsInput);
+				
+foreach ($nomsInput as $nom){
+	echo $nom;}//"<input type ='texte'";//
+	//echo "<input type ='texte'";//}
+//}
+//function afficherInput ($nomsInput){
+	//$result = "<input type ='texte'";}
+		//foreach ($nomsInput as $nom){
+		//$result ="<input type ='texte'"; 
+		//return $result;}
+	
+	
+
+
+//echo afficherInput ($nomsInput);//
