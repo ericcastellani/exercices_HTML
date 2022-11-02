6949d6c38f9127c04d1b4e97de54cda013832ad4
eric <ecastellani@outlook.fr>
2022-11-02 11:17:02 +0100

    changes

--- EX2_6.php
+++ EX2_6.php
@@ -3,13 +3,10 @@ echo "Exercice n° 6 partie 2<br>";
 $elements = ["monsieur","madame","mademoiselle"];
 function alimenterListeDeroulante ($elements){
 	$result ="";
-	foreach ($elements as $genre){
-		$result .="
-			<label for ='$genre'>$genre</label>
-			<select name = '$genre' id='$genre'>
-				<option value = '$genre'>$genre</option>
-			</select>
-			</label>";
+	foreach ($elements as $genre){$result .="<label for ='$genre'>$genre</label>
+		<select name = '$genre' id='$genre'></option>";
+	$result .="
+	<option value = '$genre'></option>";
 }
 	return $result;}
 
