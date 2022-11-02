3f2f3426c822812351519e160e77afe67bf62224
eric <ecastellani@outlook.fr>
2022-11-02 14:05:38 +0100

    ex 2_7

--- EX2_7.php
+++ EX2_7.php
@@ -1,1 +1,15 @@
 <?php
+echo"Exercice 7 partie 2 php <br>";
+
+$elements= ["Choix1","Choix2","Choix3"];
+
+function genererCheckbox($elements){
+    $result="";
+    foreach ($elements as $choix){
+        $result .="<br>
+        <input id='$choix' name = '$choix' type='checkbox' value ='$choix'>
+        <label for ='$choix'>$choix</label>";
+    }
+    return $result;
+}
+echo genererCheckbox ($elements);
