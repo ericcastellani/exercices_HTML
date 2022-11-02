017550b44c0c4057d10c5d0d35ca3cd36a54d453
eric <ecastellani@outlook.fr>
2022-11-02 11:59:24 +0100

    ex 7

--- EX2_6.php
+++ EX2_6.php
@@ -1,10 +1,10 @@
 <?php
-echo "Exercice n° 6 partie 2<br>";
-$elements = ["monsieur","madame","mademoiselle"];
+echo "Exercice n° 6 partie 2<br><br>";
+$elements = ["Monsieur","Madame","Mademoiselle"];
 $genre="monsieur";
 function alimenterListeDeroulante ($elements){
 	$result =
-	"<label for =''></label>
+	"<label for =''>Genre  :</label>
 	<select name = '' id=''>";
 	foreach ($elements as $genre){
 	$result .="
