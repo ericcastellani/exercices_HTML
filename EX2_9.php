f2e6ad2d47a02eb6cc1b89389a22b1b7820e96f2
eric <ecastellani@outlook.fr>
2022-11-02 16:21:11 +0100

    ex2_9 complete

--- EX2_9.php
+++ EX2_9.php
@@ -2,3 +2,17 @@
 echo "Exercice 9 partie 2 PHP <br>";
 
 // definition des variables
+
+$nomsRadio = ["Masculin","Féminin","Autre"];
+
+function afficherRadio($nomsRadio){
+    $result = "";
+    foreach($nomsRadio as $genre){
+        $result .="<br>
+        <input type ='radio' name = 'navig' value = '$genre'>
+        <label for ='r'>$genre</label>";
+
+    }
+    return $result;
+}
+echo afficherRadio($nomsRadio);
