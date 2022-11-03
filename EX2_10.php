9cde27a2d9f599a5c5cafc4cf55472513c6a1ba7
eric <ecastellani@outlook.fr>
2022-11-03 10:55:03 +0100

    EX2_10.php

--- EX2_10.php
+++ EX2_10.php
@@ -1,9 +1,17 @@
 <?php
 echo "Exercice 10 partie 2 PHP <br>";
 
-// Création du formulaire
+// RECUPERATION DES DONNEES DU  FORMULAIRE D'INSCRIPTION
 
-    //Etat civil
-        //Etiquettes
+$nom = $_POST["nom"];
+$prenom = $_POST["prenom"];
+$adressemail = $_POST["adressemail"];
+$ville = $_POST["ville"];
+$sexe = $_POST["sexe"];
+$metier = $_POST["metier"];
+
+// TEXTE A ENVOYER
+
+$texte = "Nom : $nom\n";
 
 
