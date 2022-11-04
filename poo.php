a04e771327b5905bbd7fcd6bdba965253520d321
eric <ecastellani@outlook.fr>
2022-11-04 16:47:03 +0100

    POO

--- poo.php
+++ poo.php
@@ -9,44 +9,7 @@
 </head>
 <body>
 	<?php
-	require 'utilisateur.class.php';
-		$pierre = new Utilisateur("Pierre", "1234", "Stras");//le mot new est utilisé pour instancier une classe cad créer une nouvelle instance d'une classe
-		$mathilde = new Utilisateur("Mathilde", "abcd", "Colmar");
-
-		//$pierre->setNom("Pierre");
-		/*->opérateur objet. Cet opérateur 
-		sert à indiquer au PHP qu'on souhaite accéder à un élement défini
-		dans notre classe via un objet créé à partir de cette classe. fa-il
-		n'y a pas de signe $ avant le nom de la propriété à laquelle on
-		 souhaite accéder */
-		//$pierre->user_pass = 'abcdef';
-
-		echo $pierre->getNom().'<br>';
-		echo $mathilde->getNom().'<br>';
-
-		$mathilde->setNom("Dupont");
-
-		echo $mathilde->getNom().'<br>';
-		echo $mathilde->getVille();
-
-
-
-
-
-
-
-
-
-	echo" bonjour";
-
-
-
-?>
-</body>
-</html>
-<?php
-
-echo "<h4> PROGRAMMATION OBJET</h4>";
+	echo "<h4> PROGRAMMATION OBJET</h4>";
 echo "En programmation orientée objet, l'objet est le nom
 que l'on donne à un élément :<br>";
 echo "- qui se touve dans une variale,<br>";
@@ -60,35 +23,56 @@ Le fait de créer un objet à partir d’une classe s’appelle instancier une c
 // INSTANCIER PREMIERE CLASSE
 
 echo "<h4>Instancier une classe</h4><br>";
-$date = new DateTime(); //ex. de class déjà existante dans php
 echo"Créer une instance, c'est créer un objet à partir du'une classe.<br>";
 echo "En PHP, cette création est le résultat de 3 opérations :<br>";
 echo"- l'allocation et l'initialisation de données,<br>";
 echo"-le stockage en mémoire du nouvel objet pour suivre son état,<br>";
 echo"-la construction d'une référence de l'objet pour le manipuler.<br>";
-
-// EXEMPLE
-	//Déclaration par référence avec le symbole &
-
-//function foo(&$var){ //indique au langage de ne pas copier la valeur la fonction mais plutôt de manipuler directement la variable d'origine
-/*	$var = 2;
-}
-$a = 1;
-foo ($a);
-echo foo($a);
-
-	//Autre exemple
-//$date="2018-02-23";
-function foo(DateTime $date){
-	$date->modify('+1 day');//permet d'ajouter 1 jour à la date
-}*/
-$date = new DateTime;
-echo $date->format('d/m/Y');
-/*
-$dateUne =new DateTime;
-$dateDeux = $dateUne;
-$dateDeux->moditfy('+1 day');
-var_dump($dateUne, $dateDeux);*/
 echo "On va déjà pouvoir créer des variables à l’intérieur de nos classes. Les variables créées dans les classes sont appelées des propriétés, afin de bien les différencier des variables « classiques » créées en dehors des classes.";
 
-echo"Une propriété, c’est donc tout simplement une variable définie dans une classe (ou éventuellement ajoutée à un objet après sa création).";
+echo"Une propriété, c’est donc tout simplement une variable définie dans une classe (ou éventuellement ajoutée à un objet après sa création).<br><br>";
+
+echo "Exemple avec new DateTime<br>";
+
+$date = new DateTime(); //ex. de class déjà existante dans php
+
+echo $date->format('d/m/Y')."<br>";
+
+//Autre exemple (https://www.pierre-giraud.com)
+
+	echo "<h3> Exemple fichier utilisateurs : https://www.pierre-giraud.com</h3><br>";
+	require 'utilisateur.class.php'; //fait appel à un fichier qui s'appelle "utilisateur.class.php"
+		$pierre = new Utilisateur("Pierre", "1234", "Stras");//le mot new est utilisé pour instancier une classe cad créer une nouvelle instance d'une classe
+		$mathilde = new Utilisateur("Mathilde", "abcd", "Colmar");
+
+		//$pierre->setNom("Pierre");
+		/*->opérateur objet. Cet opérateur 
+		sert à indiquer au PHP qu'on souhaite accéder à un élement défini
+		dans notre classe via un objet créé à partir de cette classe. fa-il
+		n'y a pas de signe $ avant le nom de la propriété à laquelle on
+		 souhaite accéder */
+		//$pierre->user_pass = 'abcdef';
+
+		echo $pierre->getNom().'<br>'; //affichera "pierre"
+		echo $mathilde->getNom().'<br>'; //affichera "Mathilde"
+
+		$mathilde->setNom("Dupont"); // set permet de changer le nom de mathilde en dupon
+
+		echo $mathilde->getNom().'<br>'; // affichera "Dupont" et non "Mathilde" car on a utilisé un set 
+		echo $mathilde->getVille();
+
+
+
+
+
+
+
+?>
+</body>
+</html>
+<?php
+
+
+
+
+
