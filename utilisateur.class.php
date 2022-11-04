6f86221cbacd18b35c939512009f9119b8a95d14
eric <ecastellani@outlook.fr>
2022-11-04 16:19:28 +0100

    fix poo

--- utilisateur.class.php
+++ utilisateur.class.php
@@ -1,7 +1,15 @@
 <?php
-class utilisateur{
+class Utilisateur{
 	private $user_name; //private : les données ne sont accessibles que depuis l'intérieur de la classe
 	private $user_pass;
+	private $ville;
+
+	public function __construct($user_name, $user_pass, $ville) //construct permet d'attribuer des valeurs aux différentes propriétés de la classe
+	{
+		$this->user_name = $user_name;
+		$this->user_pass = $user_pass;
+		$this->ville = $ville;
+	}
 
 	public function getNom(){ //ces propriétés peuvent être utiliser en dehors de la classe
 		return $this->user_name; /* $this est appelé pseudo-variable et sert à faire réf. à l'objet utilisé
@@ -18,6 +26,14 @@ class utilisateur{
 
 	public function setPasse ($new_user_pass){
 		$this->user_pass = $new_user_pass;
+	}
+
+	public function getVille() {
+		return $this->ville;
+	}
+
+	public function setVille($new_ville) {
+		$this->ville = $new_ville;
 	}
 }
 ?>
