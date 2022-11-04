d18446237ec8a7502ce2828ea3d13dbe9ae01af1
eric <ecastellani@outlook.fr>
2022-11-04 04:43:29 +0100

    EX2_11

--- EX2_11.php
+++ EX2_11.php
@@ -2,4 +2,14 @@
 echo "Exercice 11 partie 2 PHP <br>";
 
 $date = "2018-02-23";
-echo $date;
+
+echo $date;echo"<br>";
+
+// CHANGEMENT DE FORMAT
+
+$date2 = strtotime($date);
+$newdate = date("d/m/Y", $date2);
+echo $newdate;
+
+// AJOUT DU JOUR DE LA SEMAINE
+
