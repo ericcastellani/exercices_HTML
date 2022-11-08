d85c3f8b0ee3c4e003447b705856009bae106809
eric <ecastellani@outlook.fr>
2022-11-08 17:04:01 +0100

    EX2_15 fin

--- EX2_15.php
+++ EX2_15.php
@@ -4,13 +4,13 @@ $email = "elan@elan-formation.fr";
 $email2 = "contact@elan";
 
 if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-    echo'<b>'. $email.'</b>'.'  est une adresse valide <br>';
+    echo"L'adresse ".'<b>'. $email.'</b>'.'  est une adresse valide <br>';
 }else{
-    echo '<b>'.$email.'</b>.'.'est une adresse invalide <br>';   
+    echo "L'adresse ". '<b>'.$email.'</b>.'.'est une adresse invalide <br>';   
 }
 
 if(filter_var($email2, FILTER_VALIDATE_EMAIL)){
-    echo '<b>'.$email2.'</b>'.'  est une adresse valide <br>';
+    echo "L'adresse ". '<b>'.$email2.'</b>'.'  est une adresse valide <br>';
 }else{
-    echo '<b>'.$email2.'</b>'.' est une adresse invalide <br>';   
+    echo "L'adresse ".'<b>'.$email2.'</b>'.' est une adresse invalide <br>';   
 }
