11af9ddc2d4005808d3ece42b7a67dc89fb8e025
eric <ecastellani@outlook.fr>
2022-11-09 14:56:01 +0100

    EX2_13 test

--- EX2_13.php
+++ EX2_13.php
@@ -6,15 +6,34 @@ require "marques.php";
 $v1 = new Voitures("Peugeot", "408",5,0);
 $v2 = new Voitures("Citroen", "C4",3,0);
 
-echo $v1->getMarque()." ".$v1->getModele()."  ".$v1->getNbPortes()." portes <br>";
-echo $v2->getMarque()." ".$v2->getModele()."  ".$v2->getNbPortes()." portes<br>";
+echo"-->". $v1->getMarque()." ".$v1->getModele()."  ".$v1->getNbPortes()." portes <br>";
+echo"-->". $v2->getMarque()." ".$v2->getModele()."  ".$v2->getNbPortes()." portes<br>";
+echo"<br>";
+echo"-->Le Véhicule ".$v1->getMarque()." ".$v1->getModele()." démarre ";
+echo"<br>";
+$v1->demarrer();// véhicule $v1 démarre
+echo "<h4> = Statut :" .$v1->getStatut()."</h4>";//affiche le statut après démarrage
 
-echo"Le Véhicule ".$v1->getMarque()." ".$v1->getModele()." démarre <br>";
+echo"-->Le Véhicule ".$v1->getMarque()." ".$v1->getModele()." accélère de 50km/h <br>";
+$vitesse = 50; // $vitesse = vitesse d'accélération
+$v1->accelerer($vitesse);// véhicule $v1 accélère (de 50 )
+echo "-->la vitesse actuelle de ".$v1->getMarque()." ".$v1->getModele()." est de <br>"."<h4>". $v1->getVitesseActuelle()."KM/H </h4><br>";
 
+echo"-->Le Véhicule ".$v2->getMarque()." ".$v2->getModele()." démarre <br>";
 
+$v2->demarrer(); // véhicule $v2 démarre
+echo "<h4> = Statut :" .$v2->getStatut()."</h4>";
 
+$v2->stopper();// véhicule $v2 s'arrête
+echo"-->Le Véhicule ".$v2->getMarque()." ".$v2->getModele()." s'arrête <br>";
+echo "<h4> = Statut :" .$v2->getStatut()."</h4>";
+echo "<h4> = Vitesse actuelle :".$v2->getvitesseActuelle()."</h4>";
+echo"-->Le véhicule ".$v2->getMarque()." ".$v2->getModele()." veut accélérer de 20<br>";
+echo"<br>";
+$vitesse = 20;
+$v2->accelerer($vitesse);
+echo"<h4> = Vitesse actuelle : ".$v2->getVitesseActuelle()."</h4><br>";
 
-
-
-
-
+echo"------------------------------------------------------------------<br>";
+echo"<h4>La vitesse du véhicule ".$v1->getMarque()." ".$v1->getModele()." est de : ". $v1->getVitesseActuelle()." km/ h</h4><br>";
+echo"<h4>La vitesse du véhicule ".$v2->getMarque()." ".$v2->getModele()." est de : ". $v2->getVitesseActuelle()." km/ h</h4><br>";
--- marques.php
+++ marques.php
@@ -1,3 +1,5 @@
+<h4>EXERCICE 14 PARTIE 2</H4>
+
 <?php
 
 Class Voitures{
@@ -9,7 +11,7 @@ Class Voitures{
 
      // Constructeur
     public function __construct(string $_marque = "", string $_modele = "", $_nbPortes = "") {
-
+		// RMQ : on n'est pas obligé de mettre toutes les variables dans le constructeur (dans notre cas il manque statut et vitesseActuelle)
         $this->_marque = $_marque;
         $this->_modele = $_modele;
         $this->_nbPortes = $_nbPortes;
@@ -30,34 +32,56 @@ Class Voitures{
     }
     public function getVitesseActuelle(){
         return $this->_vitesseActuelle;
-
     }
-    public function setMarque() {
+	public function getStatut(){
+		if($this->_statut) {
+			$result = "Démarré";
+		} else {
+			$result = "A l'arrêt";
+		}
+		return $result;
+	}
+    public function setMarque($_marque) {
         $this->_marque = $_marque;
     }
-    public function setModele(){
+    public function setModele($_modele){
         $this->_modele =$_modele;
     }
-    public function setNbPortes(){
+    public function setNbPortes($_nbPortes){
         $this->_nbPortes = $_nbPortes;
     }
-    public function setVitesseActuelle(){
+    public function setVitesseActuelle($_vitesseActuelle){
         $this->_vitesseActuelle = $_vitesseActuelle;
     }
+	public function setStatut($_statut){
+		$this->_statut = $_statut;
+	}
 
     // METHODES démarrer, accélérer et stopper
 
-    public function Demarrer(){
+    public function demarrer(){
         $this->_statut = true;   
     }
-    public function Accelerer($vitesse){
-        //if
-        $this->_vitesseActuelle += $vitesse;
-    }
-    public function Stopper(){
-        //if
-        $this->_vitesseActuelle = 0;
+    public function accelerer($vitesse){
+        if($this->_statut){ // signifie _statut = true. Pour avoir le contraire cad false il suffiet de mettre  ! devant $this
+        	$this->_vitesseActuelle += $vitesse;}// signifie _vitesseActuelle = _vitesseActuelle + $vitesse
+		else {
+			echo "<h4>Pour accélérer, il faut démarrer le véhicule</h4>";
+		}
     }
 
+    public function stopper(){
+        if ($this->_statut){ //pour pouvoir exploiter les variables sans dollar il faut toujours passer par $this
+        	$this->_vitesseActuelle = 0;
+			$this->_statut = false;
+    	} else {
+			echo "il faut éteindre le moteur pour espérer le stopper";
+		}
 
+	}
+	public function ralentir($vitesse){
+		if($vitesse >$this->_vitesseActuelle){
+			echo "Vous ne pouvez pas ralentir d'avantage que la vitesse actuelle";
+		}else {$this->_vitesseActuelle -=$vitesse;}
+	}
 }
