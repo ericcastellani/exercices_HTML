235b33c17f039a305b7d1769d3f533c3a9b29c2e
eric <ecastellani@outlook.fr>
2022-11-09 09:49:20 +0100

    EX2_14 fin

--- EX2_14.php
+++ EX2_14.php
@@ -1,14 +1,14 @@
 <?php
 
 require "voitures.php";
-require "extends.php";
+
 
 // instanciation de 2 objets de la classe voiture 
 
 $v1 = new Voiture("Peugeot", "408");
-$velec = new Voiture("BMW", "I3 150",100);
+$velec = new VoitureElec("BMW", "I3 150",100);
 
 //Contrôle resultat
 
 echo $v1->getMarque()." ".$v1->getModele()." <br>";
-echo $velec->getMarque()." ".$velec->getModele().$velec->getAutonomie()."<br>";
+echo $velec->getMarque()." ".$velec->getModele()." Autonomie".$velec->getAutonomie()." kilomètres <br>";
