55352b59f67ea8b27e7fa3a7eeafd0903b0c30f8
eric <ecastellani@outlook.fr>
2022-11-10 11:13:52 +0100

    EX2_13 changes

--- EX2_13BIS.php
+++ EX2_13BIS.php
@@ -21,18 +21,11 @@ require "marques.php";
 // instanciation de 2 objets de la classe voiture 
 $v1 = new Voitures("Peugeot", "408",5,0);
 $v2 = new Voitures("Citroen", "C4",3,0);
-
-$resultat .="
-    <div class='card'>".
             echo"-->". $v1->getMarque()." ".$v1->getModele()."  ".$v1->getNbPortes()." portes <br>";
-            echo"-->". $v2->getMarque()." ".$v2->getModele().".  ".$v2->getNbPortes()." portes<br>";.
+            echo"-->". $v2->getMarque()." ".$v2->getModele().".  ".$v2->getNbPortes()." portes<br>";
 
 
-
-"</div>";
-{
     
-}
 
 echo"-->". $v1->getMarque()." ".$v1->getModele()."  ".$v1->getNbPortes()." portes <br>";
 echo"-->". $v2->getMarque()." ".$v2->getModele()."  ".$v2->getNbPortes()." portes<br>";
