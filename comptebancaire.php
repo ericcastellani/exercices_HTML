a1d67fd0490805e7ab4890440599ff8f0b171e39
eric <ecastellani@outlook.fr>
2022-11-15 16:21:05 +0100

    comptebancaire

---
+++ comptebancaire.php
@@ -0,0 +1,35 @@
+<?php
+
+class CompteBancaire{
+    private $_libelle;
+    private $_soldeInitial;
+    private $_devise;
+    private $_titulaire // à redéfinir en fonction de class CompteBancaire
+
+    //Constructeur
+
+public function __construct($_libelle,$_soldeInitial,$_devise,$_titulaire){//$_titulaire à redéfinir en fonction compte bancaire
+    $this-> _libelle = $_libelle;
+    $this-> _soldeInitial = $_soldeInitial;
+    $this-> _devise = $_devise;
+    $this-> _titulaire = $_titulaire;// à redéfinir  en fonction class CompteBancaire  
+}
+    // Getters et Setters
+        // Getters
+
+public function getLibelle(){
+    return $this-> _libelle;
+}
+public function getSoldeInitial(){
+    return $this-> _soldeInitial;
+}
+public function getDevise(){
+    return $this-> _devise;
+}
+public function getTitulaire(){
+    return $this-> _titulaire;// à redéfinir en fonction class CompteBancaire    
+}
+        // Setters
+
+
+}
