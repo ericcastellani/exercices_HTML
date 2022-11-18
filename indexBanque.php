71f7badbb17a9a21d20c58fb1b4ff4408c030779
eric <ecastellani@outlook.fr>
2022-11-18 10:00:03 +0100

    Débiter compte

--- indexBanque.php
+++ indexBanque.php
@@ -2,7 +2,7 @@
 
 require "comptebancaire.php";
 require "titulaire.php";
-// Déclaration des variables
+// Déclaration des variables TITULAIRES
 
 $EricCastellani = new Titulaire("CASTELLANI","Eric","1967-11-01","Compte bancaire");//toujour indiquer la date de naissance en annee- mois- jour 
 $BiduleMachin = new Titulaire("BIDULE","Machin","1900-01-01","compte bancaire");
@@ -17,11 +17,16 @@ echo $EricCastellani->calculAge()."<br>";
 echo"<br>";
 var_dump($EricCastellani);
 echo"<br>";
+
+// Déclaration des variables COMPTE BANCAIRE
+
 $Cpte1 = new CompteBancaire("compte courant",0,"€",$EricCastellani);
 $cpte2 = new CompteBancaire("Livret A",100,"€",$EricCastellani);
 $cpte3 = new CompteBancaire("compte en $",1000,"$",$EricCastellani);
 $cpte4 = new CompteBancaire("compte postal",500,"€",$BiduleMachin);
 $cpte5= new comptebancaire("compte courant",327.5,"€",$BiduleMachin);
+
+// Affichage renseignements TITULAIRE
 
 echo $EricCastellani->afficherCompteBancaire()."<br>";
 var_dump($EricCastellani)."<br>";
