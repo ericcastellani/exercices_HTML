947bc0947af0860eaed74038181e2f0fe731c350
eric <ecastellani@outlook.fr>
2022-11-22 14:54:13 +0100

    cinema genre

---
+++ genreCinema.php
@@ -0,0 +1,52 @@
+<?php
+
+//----------------------CLASSE GENRE-------------------------------------
+
+Class Genre{
+    private array $_film;
+    private string $_genre;
+
+    // constructeur
+
+public function __construct(string $_genre){
+    $this->_film = [];
+    $this->_genre = $_genre;
+
+}
+public function __toString(){
+    return $this->_genre;
+}
+
+
+//getters et setters
+	//getters
+
+public function getFilm(){
+	return $this->_film;
+}
+public function getGenre(){
+	return $this->_genre;
+}
+
+	//setters
+public function setFilm($_film){
+	$this->_film = $_film;
+}
+public function setGenre($_genre){
+	$this->_genre = $_genre;
+}
+// Méthode AddFilm
+public function addFilm(Film $film){//fonction qui permet d'ajouter un film
+    $this->_film[]=$film;
+}
+// Méthode afficher genre	
+public function afficherGenre(){
+	$result = "";
+    foreach  ($this->_film as $film){//attention on ne peut utiliser le $this que dans sa propre classe
+        $result .=$film->getFilm();
+    }
+	return $result;
+}
+
+
+}
--- indexCinema.php
+++ indexCinema.php
@@ -2,6 +2,7 @@
 <?php
 require "film.php";
 require "personneCinema.php";
+require "genreCinema.php";
 // Réalisateurs
 $ridleyScott = new Realisateur("SCOTT","Ridley","Homme","30-11-1937");
 $timburton = new Realisateur ("BURTON","Tim","Homme","25-08-1958");
@@ -20,11 +21,14 @@ $film6 = new Film("STAR WARS 4",1977,121,"Science Fiction",$georgelucas);
 $film7 = new Film("AMERICAN GRAFFITI",1973,112,"Comédie",$georgelucas);
 $film8 = new Film("STAR WARS 1",1999,133,"Science Fiction",$georgelucas);
 $film9 = new Film("STAR WARS 2",2002,142,"Science Fiction",$georgelucas);
-$film10 = new Film("STAR WARS 1",2005,140,"Science Fiction",$georgelucas);
+$film10 = new Film("STAR WARS 3",2005,140,"Science Fiction",$georgelucas);
 
 
 // TEST D'AFFICHAGE
-echo "<h3>TEST D'AFFICHAGE FILMOGRAPHIE DU REALISATEUR<br></h3>";
+
+	//affichage réalisateur
+
+echo "<h3><i>TEST D'AFFICHAGE FILMOGRAPHIE DU REALISATEUR<br></i></h3>";
 echo "<h4>$ridleyScott<br></h4>";
 echo $ridleyScott->afficherFilm();
 
@@ -33,6 +37,14 @@ echo $timburton->afficherFilm();
 
 echo "<h4>$georgelucas<br></h4>";
 echo $georgelucas->afficherFilm();
+
+	//affchage genre de films
+$film11 = new Film("BLADE RUNNER",1982,117,"Science Fiction",$ridleyScott);
+$genre1 = new Genre("FANTASTIQUE");
+$film11 = new Film("BLADE RUNNER",1982,117,$genre1,$ridleyScott);
+echo"<h4> $genre1<br></h4>";
+$genre1->afficherGenre();
+echo $film11;
 
 //Acteurs
 
