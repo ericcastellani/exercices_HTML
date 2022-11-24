777c1e774d9bd8df76d88dd4dee59153dd741030
John Doe <ecastellani@outlook.fr>
2022-11-24 11:20:31 +0100

    cinema casting

---
+++ casting.php
@@ -0,0 +1,53 @@
+<?php
+//---------------------CLASSE CASTING ------------------------------------
+
+Class Casting{
+    private string $_nom;
+    private string $_film;
+    private string $_role;
+
+
+    // constructeur
+
+public function __construct($_nom,$_film,$_role){
+    $this->_nom = $_nom;
+    $this->_film = $_film;
+    $this->_role = $_role;
+
+}
+
+	//Methode toString()
+
+public function __toString(){
+    return $this->_nom."  ".$this->_film." / ".$this->_role." <br> ";
+}
+
+	//getters et setters
+	//getters
+public function getNom(){
+	$this->_nom;
+}
+public function getFilm(){
+	$this->_film;
+}
+public function getRole(){
+	$this->_role;
+}
+	//setters
+public function setNom($_nom){
+	$this->_nom = $_nom;
+
+}
+public function setFilm($_film){
+	$this->_film = $_film;
+}
+public function setRole($_role){
+	$this->_role = $_role;
+}
+
+
+
+
+
+}
+
